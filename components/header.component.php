b440ba67dba46b8d482a3970f6037658dcf68b64
Ozbert <202310393@fit.edu.ph>
2025-05-12 18:15:36 +0800

    feat(header) - added link to exotic pets on header

--- components/header.component.php
+++ components/header.component.php
@@ -2,5 +2,7 @@
     <h1>Pet Adoption</h1>
     <nav>
         <li><a href="../../index.php">Back to Home</a></li>
+        <li><a href="../../page/common-pets/index.php">Common Pets</a></li>
+        <li><a href="../../page/exotic-pets/index.php">Exotic Pets</a></li>
     </nav>
 </header>
