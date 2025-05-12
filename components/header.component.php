43e6c4ce592d69f84640157ec3cb97ff9af04a76
Ozbert <202310393@fit.edu.ph>
2025-05-12 18:52:30 +0800

    fix(header) - added missing unordered list tag

--- components/header.component.php
+++ components/header.component.php
@@ -1,8 +1,10 @@
 <header>
     <h1>Pet Adoption</h1>
     <nav>
-        <li><a href="../../index.php">Back to Home</a></li>
-        <li><a href="../../page/common-pets/index.php">Common Pets</a></li>
-        <li><a href="../../page/exotic-pets/index.php">Exotic Pets</a></li>
+        <ul>
+            <li><a href="../../index.php">Back to Home</a></li>
+            <li><a href="../../page/common-pets/index.php">Common Pets</a></li>
+            <li><a href="../../page/exotic-pets/index.php">Exotic Pets</a></li>
+        </ul>
     </nav>
 </header>
